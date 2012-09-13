031efa033fa7e0fb2d682db8e988be1c7e483d76
anonfunc <anonfunc@gmail.com>
2012-09-13 12:58:43 -0700

    Tweaking the keychain setup.

--- home/.oh-my-zsh-custom/plugins/keychain/keychain.plugin.zsh
+++ home/.oh-my-zsh-custom/plugins/keychain/keychain.plugin.zsh
@@ -1,3 +1,7 @@
-if [ $commands[keychain] ]; then # check for keychain
-    eval $(keychain --eval --agents ssh -Q --quiet)
-fi
+__keychain_setup () {
+    if [ $commands[keychain] ]; then # check for keychain
+        eval $(keychain --eval --agents ssh -Q --quiet --ignore-missing id_rsa id_dsa)
+    fi
+}
+
+__keychain_setup
