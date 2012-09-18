ce9519b2fda80dd6dc6430d62a32f5f1a76dee60
anonfunc <anonfunc@gmail.com>
2012-09-18 14:22:49 -0700

    More aliases

---
+++ home/.oh-my-zsh-custom/aliases.zsh
@@ -0,0 +1,11 @@
+alias sr="screen -R"
+
+alias antc="ant -logger org.apache.tools.ant.listener.AnsiColorLogger"
+alias antn="ant -logger org.apache.tools.ant.NoBannerLogger"
+alias antb="ant -logger org.apache.tools.ant.listener.BigProjectLogger"
+alias antsb="ant -logger org.apache.tools.ant.listener.SimpleBigProjectLogger"
+alias ant="ant -logger org.apache.tools.ant.listener.SimpleBigProjectLogger"
+alias antp="ant -logger org.apache.tools.ant.listener.ProfileLogger"
+alias jm='eval cd $(emacsclient -e "(with-current-buffer (window-buffer (frame-selected-window)) (expand-file-name default-directory))")'
+
+
