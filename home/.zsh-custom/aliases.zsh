6b4dbbce5966bf132f1a608fc4d3e0d046201455
anonfunc <anonfunc@gmail.com>
2013-09-20 22:29:54 -0700

    New tmux aliases

--- home/.zsh-custom/aliases.zsh
+++ home/.zsh-custom/aliases.zsh
@@ -1,4 +1,5 @@
 alias jm='eval cd $(emacsclient -e "(with-current-buffer (window-buffer (frame-selected-window)) (expand-file-name default-directory))")'
 
-alias tma='tmux -2 attach -d || tmux -2'
+alias tma='tmux -2 new-session -d -t home -s "client-$$" \; set-option destroy-unattached \; attach-session -t "client-$$" || tmux -2 new-session -s home'
+alias ta='tmux -2 attach-session -t "home"'
 
