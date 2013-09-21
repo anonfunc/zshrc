e02d35596f625cfde349f49abbd73b4a1b3993ee
anonfunc <anonfunc@gmail.com>
2013-09-20 22:30:18 -0700

    PATH changes

--- home/.zsh-custom/variables.zsh
+++ home/.zsh-custom/variables.zsh
@@ -9,3 +9,5 @@ export M2=$M2_HOME/bin
 
 export PATH=$PATH:$JAVA_HOME/bin:/usr/local/bin:/usr/local/mysql/bin:/usr/local/workplace/bin
 export PATH="$HOME/bin:$PATH"
+export PATH="/export/apps/xtools/bin:$PATH"
+export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
