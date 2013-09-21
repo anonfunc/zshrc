c882c117fd34e9264bc9fe039ff0ce4acc770a3d
anonfunc <anonfunc@gmail.com>
2013-09-20 22:30:06 -0700

    Quiet keychain start

--- home/.zsh-custom/keychain.zsh
+++ home/.zsh-custom/keychain.zsh
@@ -2,7 +2,7 @@ if [[ -x /usr/bin/keychain ]]
 then
 	if [[ -f ~/.ssh/${USER}_at_workplace.com_dsa_key ]]
   then
-    /usr/bin/keychain ~/.ssh/${USER}_at_workplace.com_dsa_key
+    /usr/bin/keychain -q ~/.ssh/${USER}_at_workplace.com_dsa_key
     . ~/.keychain/`hostname`-sh
 	fi
 fi
