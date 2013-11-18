6f4ec9a35da0bd3eb4a9a8a730469171065a6319
anonfunc <anonfunc@gmail.com>
2013-11-18 11:19:23 -0800

    List daemon statuses on shell start.

---
+++ home/.zsh-custom/daemons.zsh
@@ -0,0 +1,33 @@
+echo_bad () {
+    TEXT=$1
+    shift
+    echo $* -e '\e[31m'$TEXT'\e[0m'
+}
+
+echo_good () {
+    TEXT=$1
+    shift
+    echo $* -e '\e[32m'$TEXT'\e[0m'
+}
+
+isrunning () {
+    if [[ -n $(pgrep $1) ]]
+    then
+        return 0
+    fi
+    return 1
+}
+
+showrunningstatus () {
+    for service in $*
+    do
+        if isrunning $service
+        then
+            echo_good "$service is running."
+        else
+            echo_bad "$service is not running."
+        fi
+    done
+}
+
+showrunningstatus znc btsync
