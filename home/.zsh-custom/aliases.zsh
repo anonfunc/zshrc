68fe24d7a21a105ad04ee7d82974d5231b15567e
anonfunc <anonfunc@gmail.com>
2013-11-18 11:19:40 -0800

    Shorter dir nav and listing.

--- home/.zsh-custom/aliases.zsh
+++ home/.zsh-custom/aliases.zsh
@@ -3,3 +3,4 @@ alias jm='eval cd $(emacsclient -e "(with-current-buffer (window-buffer (frame-s
 alias tma='tmux -2 new-session -d -t home -s "client-$$" \; set-option destroy-unattached \; attach-session -t "client-$$" || tmux -2 new-session -s home'
 alias ta='tmux -2 attach-session -t "home"'
 
+alias l='ls --quoting-style=shell -v --color=tty'
---
+++ home/.zsh-custom/functions.sh
@@ -0,0 +1,5 @@
+# Faster nav?
+c() {
+    cd $1 && l
+}
+compdef _dirs c
