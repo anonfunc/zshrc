93361be08d95d9e306617a078802f4b5bb00545e
anonfunc <anonfunc@gmail.com>
2013-12-04 16:59:23 -0800

    Tweak locale and add Python's user scheme to the PATH

--- home/.zsh-custom/variables.zsh
+++ home/.zsh-custom/variables.zsh
@@ -6,11 +6,15 @@ export JAVA_HOME=/export/apps/jdk/JDK-1_6_0_27
 export JDK_HOME=/export/apps/jdk/JDK-1_6_0_27
 export NLS_LANG=American_America.UTF8
 
+# For mac compatibility?
+export LANG=en_US.UTF-8
+export LC_CTYPE=en_US.UTF-8
+
 export M2_HOME=/local/maven
 export M2=$M2_HOME/bin
 
 export PATH=/usr/local/bin:/usr/local/mysql/bin:/usr/local/workplace/bin:$PATH:$JAVA_HOME/bin
-export PATH="$HOME/bin:$PATH"
+export PATH="$HOME/bin:$HOME/.local/bin:$PATH"
 #export PATH="/export/apps/xtools/bin:$PATH"
 export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
 export LEOHOME="$HOME/code/network_trunk"
