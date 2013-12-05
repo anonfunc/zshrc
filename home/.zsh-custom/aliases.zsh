f7fbc94d4812416ace997ffc12214f8bb513b307
anonfunc <anonfunc@gmail.com>
2013-12-04 18:41:43 -0800

    Better tmux command.

--- home/.zsh-custom/aliases.zsh
+++ home/.zsh-custom/aliases.zsh
@@ -1,6 +1,3 @@
 alias jm='eval cd $(emacsclient -e "(with-current-buffer (window-buffer (frame-selected-window)) (expand-file-name default-directory))")'
 
-alias tma='tmux -2 new-session -d -t home -s "client-$$" \; set-option destroy-unattached \; attach-session -t "client-$$" || tmux -2 new-session -s home'
-alias ta='tmux -2 attach-session -t "home"'
-
 alias l='ls --quoting-style=shell -v --color=tty'
---
+++ home/.zsh-custom/tmux.zsh
@@ -0,0 +1,29 @@
+tma () {
+    # cd home
+    cd
+    # Deactivate virtualenv if active
+    if [[ -n "$VIRTUAL_ENV" ]]
+    then
+        deactivate
+    fi
+
+    # How many sessions active?
+    SESSIONS=$($tmux list-sessions 2> /dev/null | wc -l)
+       # Create a sync'd client session OR create a master session.
+    if [[ $SESSIONS == 0 ]]
+    then
+        $tmux -2 new-session -s home
+    else
+        # Is home attached?
+        if $tmux list-sessions | grep home: | grep -q attached
+        then
+            # Attach a new client
+            $tmux -2 new-session -d -t home -s "client-$$" \;\
+                set-option destroy-unattached \; \
+                attach-session -t "client-$$"
+        else
+            # Attach to home.  Home isn't auto-detached, so this isn't perfect.
+            tmux -2 attach-session -t home
+        fi
+    fi
+}
